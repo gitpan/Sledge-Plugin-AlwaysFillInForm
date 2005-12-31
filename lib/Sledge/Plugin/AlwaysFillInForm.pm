da63b3c63c4265f49037d36f840fe7832a604f28
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2005-12-31 03:33:13 +0000

    Import of TOKUHIROM/Sledge-Plugin-AlwaysFillInForm-0.02 from CPAN.
    gitpan-cpan-distribution: Sledge-Plugin-AlwaysFillInForm
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TOKUHIROM/Sledge-Plugin-AlwaysFillInForm-0.02.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Sledge/Plugin/AlwaysFillInForm.pm
+++ lib/Sledge/Plugin/AlwaysFillInForm.pm
@@ -2,7 +2,7 @@ package Sledge::Plugin::AlwaysFillInForm;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 sub import {
     my $class = shift;
--- t/00_compile.t
+++ t/00_compile.t
@@ -1,4 +1,5 @@
 use strict;
 use Test::More tests => 1;
 
+sub register_hook { 'dummy function for test' }
 BEGIN { use_ok 'Sledge::Plugin::AlwaysFillInForm' }
